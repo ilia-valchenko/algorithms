ddd4b55a0eadf032dfe2b6b060b2b725f512bd48
ivalchanka <i.valchenk@hotmail.com>
2024-04-09 19:55:02 +0400

    Pushed unfinished version of the binary search algorithm.

---
+++ BinarySearch/BinarySearchHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BinarySearch
+{
+    public static class BinarySearchHelper<T> where T: struct, IEquatable<T>, IComparable<T>
+    {
+        public static int? Search(T[] array, T item, int iterationNumber = 0)
+        {
+            // Let's allow users passing unordered array.
+            // array = array.OrderBy(x => x).ToArray();
+
+            var commaSeparatedArrayItems = string.Join(",", array.Select(x => x.ToString()));
+            Debug.WriteLine($"Array: [{commaSeparatedArrayItems}].");
+
+            int low = 0;
+            int high = array.Length - 1;
+
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+                var possibleValue = array[mid];
+                var comparisonResult = item.CompareTo(possibleValue);
+                T[] newArray = null;
+
+                Debug.WriteLine(
+                    $"Start doing {++iterationNumber} iteration. Position: {mid}. Possible value: {possibleValue.ToString()}. Comparison result: {comparisonResult}.");
+
+                if (comparisonResult == 0)
+                {
+                    Debug.WriteLine($"The item was found! It's {possibleValue}.");
+                    return mid;
+                }
+                else if (comparisonResult < 0) // The instance precedes
+                {
+                    high = mid - 1;
+                    newArray = array.Take(high - low).ToArray();
+                }
+                else // The instance follows
+                {
+                    low = mid + 1;
+                    newArray = array.Skip(mid).Take(high - low).ToArray();
+                }
+
+                return Search(newArray, item, iterationNumber);
+            }
+
+            Debug.WriteLine("The item was not found in the array!");
+            return null;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using BinarySearch;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============== Algorithms ==============");
+
+            var value1 = 41;
+            var value2 = 4921;
+            var array = new[] { 1, 5, 6, 5, 89, 8, 51, 5, 8, value2, 95, 6, 4, 1265, 4596, 165, 49, value1, 6594 };
+            array = array.OrderBy(x => x).ToArray();
+
+            var result = BinarySearchHelper<int>.Search(array, value2);
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey();
+        }
+    }
+}
